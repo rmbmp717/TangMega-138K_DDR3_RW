95620fbb7c7a4bad9010751accc16a859fafff5b
nishiharu <179163361+rmbmp717@users.noreply.github.com>
2024-11-30 23:27:46 +0900

    Add files via upload

---
+++ src/ddr3_test.v
@@ -0,0 +1,236 @@
+module ddr3_test (
+    input clk,
+    input rst_n,
+    input app_rdy,
+    output reg [2:0] app_cmd,
+    output reg app_en,
+    output reg [29-1:0] app_addr,
+    input wr_data_rdy,
+    output reg [256-1:0] app_wdf_data,
+    output reg app_wdf_wren,
+    output reg app_wdf_end,
+    output [32-1:0] app_wdf_mask,
+    output app_burst,
+    input app_rd_data_valid,
+    input [256-1:0] app_rd_data,
+    input init_calib_complete,
+    output wdone,
+    output rdone,
+    output reg [23:0] num_ok,
+    output reg [23:0] num_ng,
+    output [2:0] test_state
+);
+
+assign app_wdf_mask = 0;
+assign app_burst = 0;
+
+reg [2:0] state;
+
+reg         data_ok;
+reg         data_err;
+wire [63:0] read_data;
+assign      read_data = app_rd_data[63:0];
+
+wire [31:0] d32_app_addr;
+assign      d32_app_addr = {{16'd0},{app_addr[15:0]}};
+
+wire [15:0] app_addr_m;
+assign      app_addr_m = app_addr - 8;
+
+wire [31:0] d32_app_addr_m;
+assign      d32_app_addr_m = {{16'd0},{app_addr_m[15:0]}};
+
+localparam STATE_IDLE = 0;
+localparam STATE_WRITE = 1;
+localparam STATE_WRITE_WAIT = 2;
+localparam STATE_READ_CHECK = 3;
+localparam STATE_READ_CMD = 4;
+localparam STATE_FINISH = 5;
+
+reg [15:0] cnt;
+
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        state <= STATE_IDLE;
+        app_cmd <= 0;
+        app_en <= 0;
+        app_wdf_data <= 0;
+        app_wdf_wren <= 0;
+        app_wdf_end <= 0;
+        app_addr <= 0;
+        num_ok <= 0;
+        num_ng <= 0;
+        cnt <= 0;
+        data_ok <= 0;
+        data_err <= 0;
+    end
+    else begin
+        case(state)
+            STATE_IDLE: begin
+                if(init_calib_complete) begin
+                    if(cnt >= 65535) begin
+                        state <= STATE_WRITE;
+                        cnt <= 0;
+                    end
+                    else begin
+                        state <= state;
+                        cnt <= cnt + 1;
+                    end
+                end
+                else begin
+                    state <= state;
+                    cnt <= cnt;
+                end
+                app_cmd <= 0;
+                app_en <= 0;
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+                app_addr <= 0;
+                num_ok <= 0;
+                num_ng <= 0;
+            end
+            STATE_WRITE: begin
+                data_ok <= 0;
+                data_err <= 0;
+                if(app_rdy & wr_data_rdy) begin
+                    if(app_addr >= 2**27-8) begin
+                        app_cmd <= 0;
+                        app_en <= 0;
+                        app_wdf_data <= 0;
+                        app_wdf_wren <= 0;
+                        app_wdf_end <= 0;
+                        app_addr <= 0;
+                        state <= STATE_READ_CMD;
+                    end
+                    else begin
+                        app_cmd <= 0;
+                        app_en <= 1;
+                        //app_wdf_data <= 256'hAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_1234;
+                        app_wdf_data <= {{224'hAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA},{d32_app_addr}};
+                        app_wdf_wren <= 1;
+                        app_wdf_end <= 1;
+                        app_addr <= app_addr + 8;
+                        state <= STATE_WRITE_WAIT;
+                    end
+                end
+                else begin
+                    app_cmd <= 0;
+                    app_en <= 0;
+                    app_wdf_data <= 0;
+                    app_wdf_wren <= 0;
+                    app_wdf_end <= 0;
+                    app_addr <= app_addr;
+                    state <= state;
+                end
+                num_ok <= 0;
+                num_ng <= 0;
+            end
+            STATE_WRITE_WAIT: begin
+                if(cnt >= 15) begin
+                    state <= STATE_WRITE;
+                    cnt <= 0;
+                end
+                else begin
+                    state <= state;
+                    cnt <= cnt + 1;
+                end
+                app_cmd <= 0;
+                app_en <= 0;
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+                app_addr <= app_addr;
+                num_ok <= 0;
+                num_ng <= 0;
+            end
+            STATE_READ_CHECK: begin
+                if(app_rd_data_valid) begin
+                    //if(app_rd_data == 256'hAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_1234) begin
+                    //if(app_rd_data[255:16] == {{224'hAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA},{d32_app_addr[31:16]}}) begin
+                    if(app_rd_data == {{224'hAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA},{d32_app_addr_m[31:0]}}) begin
+                        num_ok <= num_ok + 1;
+                        data_ok <= 1;
+                        data_err <= 0;
+                    end
+                    else begin
+                        num_ok <= num_ok;
+                        num_ng <= num_ng + 1;
+                        data_ok <= 0;
+                        data_err <= 1;
+                    end
+                    state <= STATE_READ_CMD;
+                end
+                else begin
+                    num_ok <= num_ok;
+                    state <= state;
+                    data_ok <= 0;
+                    data_err <= 0;
+                end
+                app_cmd <= 0;
+                app_en <= 0;
+                app_addr <= app_addr;
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+            end
+            STATE_READ_CMD: begin
+                if(app_rdy) begin
+                    if(app_addr >= 2**27-8) begin
+                        app_cmd <= 0;
+                        app_en <= 0;
+                        app_addr <= 0;
+                        state <= STATE_FINISH;
+                    end
+                    else begin
+                        app_cmd <= 1;
+                        app_en <= 1;
+                        app_addr <= app_addr + 8;
+                        state <= STATE_READ_CHECK;
+                    end
+                end
+                else begin
+                    app_cmd <= 0;
+                    app_en <= 0;
+                    app_addr <= app_addr;
+                    state <= state;
+                end
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+                num_ok <= num_ok;
+                data_ok <= 0;
+                data_err <= 0;
+            end
+            STATE_FINISH: begin
+                //state <= state;
+                state <= STATE_WRITE;
+                app_cmd <= 0;
+                app_en <= 0;
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+                //app_addr <= app_addr;
+                app_addr <= 0;
+                num_ok <= num_ok;
+            end
+            default: begin
+                state <= state;
+                app_cmd <= 0;
+                app_en <= 0;
+                app_wdf_data <= 0;
+                app_wdf_wren <= 0;
+                app_wdf_end <= 0;
+                app_addr <= app_addr;
+                num_ok <= num_ok;
+            end
+        endcase
+    end
+end
+
+assign wdone = (state == STATE_READ_CHECK) | (state == STATE_READ_CMD) | (state == STATE_FINISH);
+assign rdone = state == STATE_FINISH;
+
+assign test_state = state;
+
+endmodule
---
+++ src/top.v
@@ -0,0 +1,145 @@
+/*
+DDR3 Test Project
+*/
+module top(
+    input sys_clk, // 50M
+    input rst_n,
+    output[7:0] led,
+
+    output [14-1:0] ddr_addr,
+    output [3-1:0] ddr_bank,
+    output ddr_cs,
+    output ddr_ras,
+    output ddr_cas,
+    output ddr_we,
+    output ddr_ck,
+    output ddr_ck_n,
+    output ddr_cke,
+    output ddr_odt,
+    output ddr_reset_n,
+    output [4-1:0] ddr_dm,
+    inout  [32-1:0] ddr_dq,
+    inout  [4-1:0] ddr_dqs,
+    inout  [4-1:0] ddr_dqs_n
+);
+
+assign  led[0]  = rst_n;
+assign  led[1]  = test_state[0];
+
+wire clk100M;
+
+reg uart_wr;
+wire tx_done;
+reg [7:0] uart_wdata;
+wire tx_empty;
+wire tx_full;
+wire [7:0] tx_data;
+wire b_tick;
+
+wire pll_lock;
+wire memory_clk;
+wire pll_stop;
+
+Gowin_PLL upll(
+    .lock(pll_lock),
+    .clkout0(),
+    .clkout1(clk100M),
+    .clkout2(memory_clk),
+    .clkin(sys_clk),
+    .reset(~rst_n),
+    .enclk0(1'b1),
+    .enclk1(1'b1),
+    .enclk2(pll_stop)
+);
+
+wire init_calib_complete;
+wire app_rdy;
+wire app_wdf_rdy;
+
+wire clk_x1;
+wire [2:0] app_cmd;
+wire app_en;
+wire [29-1:0] app_addr;
+wire [256-1:0] app_wdf_data;
+wire app_wdf_wren;
+wire app_wdf_end;
+wire [32-1:0] app_wdf_mask;
+wire [256-1:0] app_rd_data;
+wire app_rd_data_valid;
+wire app_burst;
+
+DDR3_Memory_Interface_Top u_ddr3 (
+    .memory_clk      (memory_clk),
+    .pll_stop        (pll_stop),
+    .clk             (clk100M),
+    .rst_n           (rst_n),
+    .cmd_ready       (app_rdy),
+    .cmd             (app_cmd),
+    .cmd_en          (app_en),
+    .addr            (app_addr),
+    .wr_data_rdy     (app_wdf_rdy),
+    .wr_data         (app_wdf_data),
+    .wr_data_en      (app_wdf_wren),
+    .wr_data_end     (app_wdf_end),
+    .wr_data_mask    (app_wdf_mask),
+    .rd_data         (app_rd_data),
+    .rd_data_valid   (app_rd_data_valid),
+    .rd_data_end     (),
+    .sr_req          (1'b0),
+    .ref_req         (1'b0),
+    .sr_ack          (),
+    .ref_ack         (),
+    .init_calib_complete(init_calib_complete),
+    .clk_out         (clk_x1),
+    .pll_lock        (pll_lock),
+    .burst           (app_burst),
+    .ddr_rst         (),
+    // mem interface
+    .O_ddr_addr      (ddr_addr),
+    .O_ddr_ba        (ddr_bank),
+    .O_ddr_cs_n      (ddr_cs),
+    .O_ddr_ras_n     (ddr_ras),
+    .O_ddr_cas_n     (ddr_cas),
+    .O_ddr_we_n      (ddr_we),
+    .O_ddr_clk       (ddr_ck),
+    .O_ddr_clk_n     (ddr_ck_n),
+    .O_ddr_cke       (ddr_cke),
+    .O_ddr_odt       (ddr_odt),
+    .O_ddr_reset_n   (ddr_reset_n),
+    .O_ddr_dqm       (ddr_dm),
+    .IO_ddr_dq       (ddr_dq),
+    .IO_ddr_dqs      (ddr_dqs),
+    .IO_ddr_dqs_n    (ddr_dqs_n)
+);
+
+wire wdone;
+wire rdone;
+wire [23:0] num_ok;
+wire [23:0] num_ng;
+
+wire [2:0] test_state;
+
+ddr3_test u_ddr_test(
+    .clk                (clk_x1),
+    .rst_n              (rst_n),  
+    .app_rdy            (app_rdy),
+    .app_cmd            (app_cmd),
+    .app_en             (app_en),
+    .app_addr           (app_addr),
+    .wr_data_rdy        (app_wdf_rdy),
+    .app_wdf_data       (app_wdf_data),
+    .app_wdf_wren       (app_wdf_wren),
+    .app_wdf_end        (app_wdf_end),
+    .app_wdf_mask       (app_wdf_mask),
+    .app_burst          (app_burst),
+    .app_rd_data_valid  (app_rd_data_valid),
+    .app_rd_data        (app_rd_data), 
+    .init_calib_complete(init_calib_complete),
+    .wdone              (wdone),
+    .rdone              (rdone),
+    .num_ok             (num_ok),
+    .num_ng             (num_ng),
+    .test_state         (test_state)
+);
+
+endmodule
